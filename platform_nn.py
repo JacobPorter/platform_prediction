5af2ae93a68c75daab750315da9f545da5af594d
Jacob Porter <jsporter@virginia.edu>
2020-09-22 19:26:43 -0400

    Initial NN model.

--- platform_nn.py
+++ platform_nn.py
@@ -10,9 +10,20 @@ import argparse
 import datetime
 import sys
 
+from keras.layers import Dense
+from keras.models import Sequential
 
-def train():
-    pass
+
+def train(X, y, input_dim=36, output_dim=5, epochs=25, batch_size=64):
+    model = Sequential()
+    model.add(Dense(12, input_dim=input_dim, activation='relu'))
+    model.add(Dense(8, activation='relu'))
+    model.add(Dense(output_dim, activation='softmax'))
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+    model.fit(X, y, epochs=epochs, batch_size=batch_size)
+    model.save("model.h5")
 
 
 def predict():
