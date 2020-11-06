2dfccdabba7d0d4970abe5495392ffcfbed6bcf5
Jacob Porter <jsporter@virginia.edu>
2020-11-06 01:16:39 -0500

    Added command to predict the platform.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # platform_prediction
 Predict sequencing platform.
+The models may need to be un-gzipped to be used.
--- lib/platform_features.py
+++ lib/platform_features.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-Extract features useful for machine learning for platform prediction.
+Extract features from the phred quality string.
 
 :Authors:
     Jacob Porter <jsporter@virginia.edu>
@@ -12,7 +12,7 @@ import math
 import sys
 from collections import Counter
 
-from SeqIterator.SeqIterator import SeqReader
+from lib.SeqIterator.SeqIterator import SeqReader
 
 # String of all of the quality characters.
 QUAL_STR = '!"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~'
@@ -83,12 +83,50 @@ def get_offset(qual):
 def get_features(fastq_input,
                  label=None,
                  subportions=3,
-                 positions=(None, None),
+                 positions=(0, 0),
                  header=False,
                  reduced=True,
                  output=sys.stdout,
                  debug=False):
-    """Get the features from all sequences in the file. Print them."""
+    """
+    Get the features from sequences in the fastq file.
+
+    Parameters
+    ----------
+    fastq_input: str
+        The location of a fastq file.
+    label: str or None
+        The platform of the fastq file.
+        Use None if the platform is unknown.  For example, for prediction.
+    subportions: int
+        The number of subportions to extract features.
+    positions: (int, int)
+        The beginning and ending of positions of fastq sequences.
+        Use (0, 0) to get the whole file.
+    header: bool
+        If True, a header labeling the features will be given.
+        If False, no header will be given.
+    reduced: bool
+        If True, use a reduced feature set. (recommended)
+        If False, use all features.  This could result in bad numbers.
+    output: writeable or None
+        If a writeable is given, write the output to this 'file'.
+        If None is given, the features will be returned in a list.
+    debug: bool
+        Controls additional debug information.
+
+    Returns
+    -------
+    count: int
+        An integer of the number of sequences processed.
+    all_features: lst
+        A list of features.  (output must be None)
+    all_labels: lst
+        A list of labels.  (output must be None)
+    my_header: lst
+        A list of strings labeling each column.  (output must be None.)
+
+    """
     def get_qual_features(qual_ascii, reduced=False):
         seq_qual_prob = list(
             map(lambda x: transform_phred_to_prob(x, offset=offset),
@@ -98,24 +136,27 @@ def get_features(fastq_input,
     reader = SeqReader(fastq_input, file_type='fastq')
     position = 0
     x, y = positions[0], positions[1]
+    if x and y and y < x:
+        x, y = y, x
     count = 0
+    my_header = []
     if header:
         if reduced:
             my_header = HEADER[0:4]
         else:
-            my_header = HEADER
-        output.write("\t".join(my_header))
-        if subportions > 1:
-            output.write("\t")
+            my_header = HEADER.copy()
         for i in range(subportions + subportions - 1):
             for item in my_header:
-                output.write(item + "_" + str(i + 1) + "\t")
+                my_header.append(item + "_" + str(i + 1))
         if label:
-            output.write("label")
-            output.write("\n")
+            my_header.append("label")
+        if output:
+            print("\t".join(my_header), file=sys.stdout)
+    all_features = []
+    all_labels = []
     for record in reader:
         features = []
-        header, read, qual, _ = record
+        _, read, qual, _ = record
         position += 1
         if len(qual) == 0 or (x and (position < x or position > y)):
             continue
@@ -140,10 +181,30 @@ def get_features(fastq_input,
                 sys.stderr.flush()
             features += get_qual_features(qual[i * halflength:finallength],
                                           reduced)
-        if label:
+        if label and output:
             features.append(label)
-        print("\t".join(map(str, features)), file=output)
+        elif label and not output:
+            all_labels.append(label)
+        if output:
+            print("\t".join(map(str, features)), file=output)
+        else:
+            all_features.append(features)
+    if not output:
+        return count, all_features, all_labels, my_header
     return count
+
+
+def nonnegative(value):
+    """Check that value is a non-negative integer."""
+    my_error = argparse.ArgumentTypeError(
+        "%s is not a non-negative integer value" % value)
+    try:
+        my_value = int(value)
+    except ValueError:
+        raise my_error
+    if my_value < 0:
+        raise my_error
+    return my_value
 
 
 def main():
@@ -161,14 +222,13 @@ def main():
                         type=int,
                         help=('The number of subportions to divide into.'),
                         default=3)
-    parser.add_argument(
-        "--range",
-        "-r",
-        nargs=2,
-        type=int,
-        help=('The range of reads to sample.  '
-              'To process the whole file, do not include an argument.'),
-        default=(None, None))
+    parser.add_argument("--range",
+                        "-r",
+                        nargs=2,
+                        type=nonnegative,
+                        help=('The range of reads to sample.  '
+                              'To process the whole file, use "0 0".'),
+                        default=(0, 0))
     parser.add_argument(
         "--header",
         "-d",
