143a17af0573a303bce9b20226ecaa5014f075ea
Jacob Porter <jsporter@virginia.edu>
2022-06-01 21:09:51 -0400

    Corrected PYTHONPATH.

--- make.sh
+++ make.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cwd=$(pwd)
 echo "#!/bin/sh" > predict_platform
-echo "export PYTHON_PATH=$cwd/lib/:\$PYTHON_PATH" >> predict_platform
+echo "export PYTHONPATH=$cwd/lib/:\$PYTHONPATH" >> predict_platform
 echo "export PLATFORM_MODEL=$cwd/models/reduced/RandomForestClassifier/" >> predict_platform
 echo "python3 $cwd/predict_platform.py \"\$@\"" >> predict_platform
 chmod 755 predict_platform
